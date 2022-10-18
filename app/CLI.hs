c68523182a1949d42f0df3d1be3f79532290cec0
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-19 00:13:31 +1100

    added CLI stuff

--- app/CLI.hs
+++ app/CLI.hs
@@ -1,2 +1,13 @@
 module CLI where 
+import Options.Applicative
+
+data CLIOptions = CLIOptions { configFile :: Maybe FilePath }
+
+data Commands 
+  = Add 
+  | Start 
+  | Stop 
+  | Projects
+
+
 
