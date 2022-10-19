b187cf96dae55fa9feab3b6b3856838584801a2c
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-19 15:44:03 +1100

    more code

--- app/CLI.hs
+++ app/CLI.hs
@@ -1,13 +1,105 @@
-module CLI where 
-import Options.Applicative
+module CLI where
 
-data CLIOptions = CLIOptions { configFile :: Maybe FilePath }
+import Options.Applicative hiding (command)
+import qualified Options.Applicative as O
 
-data Commands 
-  = Add 
-  | Start 
-  | Stop 
-  | Projects
+data CLIOptions = CLIOptions {configFile :: Maybe FilePath, command :: Command}
+  deriving (Show, Eq)
 
+cliOptions :: Parser CLIOptions
+cliOptions =
+  CLIOptions
+    <$> optional
+      ( strOption
+          ( long "config"
+              <> metavar "CONFIG"
+          )
+      )
+    <*> parseCommand
 
+type From = String
 
+type To = String
+
+type Project = String
+
+type Tag = String
+
+type Tags = [Tag]
+
+data AddOptions = AddOptions From To Project Tags
+  deriving (Show, Eq)
+
+data StartOptions = StartOptions Project Tags
+  deriving (Show, Eq)
+
+data StopOptions = StopOptions
+  deriving (Show, Eq)
+
+data Command
+  = Add AddOptions
+  | Start StartOptions
+  | Stop StopOptions
+  deriving (Show, Eq)
+
+parseCommand :: Parser Command
+parseCommand =
+  subparser
+    ( O.command "add" (info addCommand (progDesc "Add a new frame to the existing database"))
+        <> O.command "start" (info startCommand (progDesc "Start time monitoring on a project"))
+        <> O.command "stop" (info stopCommand (progDesc "Stop the ongoing time monitoring"))
+    )
+
+addOptions :: Parser AddOptions
+addOptions =
+  AddOptions
+    <$> strOption
+      ( long "from"
+          <> metavar "TIME"
+          <> help "Target for the starting time"
+      )
+    <*> strOption
+      ( long "to"
+          <> metavar "TIME"
+          <> help "Target for the ending time"
+      )
+    <*> strOption
+      ( long "project"
+          <> metavar "PROJECT"
+          <> help "Name of the project to add a new time frame to"
+      )
+    <*> many
+      ( strOption
+          ( long "tag"
+              <> short 't'
+              <> metavar "TAG"
+          )
+      )
+
+startOptions :: Parser StartOptions
+startOptions =
+  StartOptions
+    <$> strOption
+      ( long "project"
+          <> metavar "PROJECT"
+          <> help "Name of the project to start"
+      )
+    <*> many
+      ( strOption
+          ( long "tag"
+              <> short 't'
+              <> metavar "TAG"
+          )
+      )
+
+stopOptions :: Parser StopOptions
+stopOptions = pure StopOptions
+
+addCommand :: Parser Command
+addCommand = Add <$> addOptions
+
+startCommand :: Parser Command
+startCommand = Start <$> startOptions
+
+stopCommand :: Parser Command
+stopCommand = Stop <$> stopOptions
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main (main) where
-import CLI 
+
+import CLI
+import Options.Applicative hiding (command)
 import TUI
 
 main :: IO ()
-main = putStrLn "Hello World"
+main = runHTime =<< execParser opts
+  where
+    opts =
+      info
+        (cliOptions <**> helper)
+        ( fullDesc
+            <> progDesc "HTime is a haskell time monitoring tool very much inspired from Watson"
+            <> header "λ :: HTime"
+        )
+
+runHTime :: CLIOptions -> IO ()
+runHTime c = do
+  print c
